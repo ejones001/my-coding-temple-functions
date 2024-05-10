145544230cd6070930fcd1f3b6182387c2184e9e
ejones001 <ejonesnavy@gmail.com>
2024-05-10 03:22:24 -0500

    Calculator App task 1

---
+++ CalcApp.py
@@ -0,0 +1,18 @@
+# Function for addition
+def add(x, y):
+    return x + y
+
+# Function for subtraction
+def subtract(x, y):
+    return x - y
+
+# Function for multiplication
+def multiply(x, y):
+    return x * y
+
+# Function for division
+def divide(x, y):
+    if y == 0:
+        return "Error: Division by zero!"
+    else:
+        return x / y
