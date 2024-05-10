1cbaa4a713489e2bfaa59b37d00df87080ef53a9
ejones001 <ejonesnavy@gmail.com>
2024-05-10 03:26:18 -0500

    Calculator App task 2

---
+++ CalcApp_Task2.py
@@ -0,0 +1,37 @@
+
+# Function for addition
+def add(x, y):
+    return x + y
+
+# Function for subtraction
+def subtract(x, y):
+    return x - y
+
+# Function for multiplication
+def multiply(x, y):
+    return x * y
+
+# Function for division
+def divide(x, y):
+    if y == 0:
+        return "Error: Division by zero!"
+    else:
+        return x / y
+
+
+# Get user input for numbers and operation choice
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operation = input("Enter operation (+ for addition, - for subtraction, * for multiplication, / for division): ")
+
+# Perform the chosen operation
+if operation == '+':
+    print("Result:", add(num1, num2))
+elif operation == '-':
+    print("Result:", subtract(num1, num2))
+elif operation == '*':
+    print("Result:", multiply(num1, num2))
+elif operation == '/':
+    print("Result:", divide(num1, num2))
+else:
+    print("Invalid operation!")
