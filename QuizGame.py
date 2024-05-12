081702d40f2a4d9c5aeed38cfce25165f75303f5
ejones001 <ejonesnavy@gmail.com>
2024-05-11 21:58:16 -0500

    Quiz game task 1

---
+++ QuizGame.py
@@ -0,0 +1,18 @@
+# List of questions and answers
+quiz_questions = [
+    {
+        "question": "What is the capital of France?",
+        "answer": "Paris"
+    },
+    {
+        "question": "How many planets are in the solar system? (Enter a number)",
+        "answer": "9",
+        "answer": "8"
+    },
+    {
+        "question": "Who is one of the founders of Apple?",
+        "answer": "Steve Jobs",
+        "answer": "Steve Wozniak",
+        "answer": "Ronald Wayne"
+    }
+]
